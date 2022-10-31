6c02a03c8a77539cfe24de5544beb3d1a1b6756d
joao-loyola <jpala13@gmail.com>
2022-10-31 16:19:52 -0300

    novas bases

--- PreProcess/order.py
+++ PreProcess/order.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("../Data/SE_filter50.csv")
+df = pd.read_csv("../Data/SE.csv")
 half = df.shape[0]/2
 
 new = df.iloc[:, :9097]
@@ -10,6 +10,7 @@ se = abs(np.sum(df.iloc[:, 9097:]) - half)
 order_se = se.sort_values().index
 
 new[order_se] = df[order_se]
+new = new.iloc[:, :9107]
 
 print(new.iloc[:, 9097:])
-# new.to_csv("../Data/SE_filter50_order.csv", index=False)
+# new.to_csv("../Data/SE_order_top10.csv", index=False)
---
+++ PreProcess/top.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("../Data/SE.csv")
+
+new = df.iloc[:, :9097]
+
+se = np.sum(df.iloc[:, 9097:])
+order_se = se.sort_values(ascending=False).index
+
+new[order_se] = df[order_se]
+new = new.iloc[:, :9197] # 9107, 9147, 9197
+
+print(new.iloc[:, 9097:])
+# new.to_csv("../Data/SE_top100.csv", index=False)
